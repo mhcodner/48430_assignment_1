2ffc36a261537100c47cc493aaef47cbfce8435b
Michael Codner <mhcodner@gmail.com>
2016-03-29 21:12:10 +1100

    added comments

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ int triangularNumber (int n) {
 }
 
 int printEvenTriangle (int size) {
-    int highest = 1;
+    int highest = 1; // Store the highest even number
     for (int i = 1; i < size; i++) {
-        int triangle = triangularNumber(i);
-        if (triangle % 2 == 0 && triangle < size) {
+        int triangle = triangularNumber(i); // Get triangle number for current iteration
+        if (triangle % 2 == 0 && triangle < size) { // Check if the triangle number is even and less than the input
             printf("\t%d", triangle);
             highest = triangle;
         }
@@ -21,11 +21,11 @@ int printEvenTriangle (int size) {
 
 void printTriangle (int size) {
     for (int i = 1; i < size; i++) {
-        int triangle = triangularNumber(i);
-        char pad[128];
+        int triangle = triangularNumber(i); // Get triangle number for current iteration
+        char pad[128]; // The string used to left pad the output
         if (triangle < size) {
             printf("%s%02d \n", pad, triangle);
-            strcat(pad, "** ");
+            strcat(pad, "** "); // Add '** ' to the string in each iteration
         }
     }
 }
@@ -33,10 +33,10 @@ void printTriangle (int size) {
 int main() {
     int size;
     printf("Enter a number between 0 and 200>");
-    scanf("%d", &size);
+    scanf("%d", &size); // Receive input and store in the 'size' integer
     printf("Printing even triangular numbers below %d:\n", size);
-    int highest = printEvenTriangle(size);
+    int highest = printEvenTriangle(size); // Print the even triangle numbers and store the highest number
     printf("The last even triangular number is %d. Printing the triangle:\n", highest);
-    printTriangle(size);
+    printTriangle(size); // Print the whole triangle
     return 0;
 }
