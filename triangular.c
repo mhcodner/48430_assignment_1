0bc1cb359aa2a5796d244c0ea81dc9750a4d3772
Michael Codner <mhcodner@gmail.com>
2016-04-04 15:02:36 +1000

    resubmit with todays date

--- triangular.c
+++ triangular.c
@@ -2,7 +2,7 @@
 48430 Embedded C - Assignment 1
 Name: Michael Codner
 Student ID: 11989668
-Date of submission: 30/03/2016
+Date of submission: 04/04/2016
 A brief statement on what you could achieve (less than 50 words):
 Everything
 
